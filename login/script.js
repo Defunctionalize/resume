e4fc8f0ca7e4bf04decbf77191d455272c6694d9
Tyler Tolton <tjtolton@gmail.com>
2019-10-27 20:36:10 +0000

    disable redirect for testing

--- login/script.js
+++ login/script.js
@@ -9,9 +9,9 @@
                 IdentityPoolId: 'us-east-1:573e1ed2-cc7e-4315-8714-199a170ea0d3',
                 Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": (new URLSearchParams(window.location.search)).get("id_token")}
             });
-	alert(AWS.config.credentials.accessKeyId);
-            
-            window.location.replace("https://us-east-1.console.aws.amazon.com/cloud9/ide/5f616193b5754e1b92efc204a4e48917");
+		console.log(AWS.config.credentials.accessKeyId);
+            	console.log("testtest");
+           // window.location.replace("https://us-east-1.console.aws.amazon.com/cloud9/ide/5f616193b5754e1b92efc204a4e48917");
                   
         }
         catch (e) {
