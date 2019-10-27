7a48937fdbebed79a620904f8068319eee478a0e
Tyler Tolton <tjtolton@gmail.com>
2019-10-27 20:30:14 +0000

    alert accesskeyid as test

--- login/script.js
+++ login/script.js
@@ -5,10 +5,11 @@
         console.log("hello");
         try {
             AWS.config.region = 'us-east-1';
-            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+             AWS.config.credentials= new AWS.CognitoIdentityCredentials({
                 IdentityPoolId: 'us-east-1:573e1ed2-cc7e-4315-8714-199a170ea0d3',
                 Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": (new URLSearchParams(window.location.search)).get("id_token")}
             });
+	alert(AWS.config.credentials.accessKeyId);
             
             window.location.replace("https://us-east-1.console.aws.amazon.com/cloud9/ide/5f616193b5754e1b92efc204a4e48917");
                   
