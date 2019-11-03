30331e827aabf72e6909b7dfd2a6b3bee757986a
Tyler Tolton <tjtolton@gmail.com>
2019-11-03 00:13:02 +0000

    update login script

--- login/script.js
+++ login/script.js
@@ -5,15 +5,15 @@
         console.log("hello");
         try {
 		AWS.config.region = 'us-east-1';
-		console.log('about to try the awscognitoidentitycredentials call:');
+		console.log('about to try the awscognitoidentitycredentials call... ');
+		let id_token=(new URLSearchParams(window.location.search)).get("id_token");
+		console.log('ID Token: ', id_token);
 		AWS.config.credentials= new AWS.CognitoIdentityCredentials({
                 IdentityPoolId: 'us-east-1:573e1ed2-cc7e-4315-8714-199a170ea0d3',
-                Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": (new URLSearchParams(window.location.search)).get("id_token")}
+                Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": id_token}
             });
-		console.log(AWS.config.credentials.accessKeyId);
-            	console.log("testtest");
-           // window.location.replace("https://us-east-1.console.aws.amazon.com/cloud9/ide/5f616193b5754e1b92efc204a4e48917");
-                  
+        let refresh = AWS.config.credentials.getPromise();
+        refresh.then(() => console.log("creds? ---", AWS.console.credentials.accessKeyId));
         }
         catch (e) {
             console.log(e);
