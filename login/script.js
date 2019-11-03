8ea8dd57ce2ac79397361e1743655095f651910b
Tyler Tolton <tjtolton@gmail.com>
2019-11-03 23:38:32 +0000

    call lambda with credentials

--- login/script.js
+++ login/script.js
@@ -1,31 +1,31 @@
 var hash = () => Object.fromEntries(window.location.hash.substr(1).split("&").map(x => x.split("=")))
-function fixedEncodeURIComponent(str) {
-  return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
-    return '%' + c.charCodeAt(0).toString(16);
-  });
-}
+
 function test() {
     console.log = (...x) => alert(x);
     console.error = console.log;
     console.warn = console.log;
-    console.log("hello");
     try {
-	AWS.config.region = 'us-east-1';
-	let id_token=hash().id_token;
-    console.log('ID Token', id_token);
-    AWS.config.credentials= new AWS.CognitoIdentityCredentials({
-            IdentityPoolId: 'us-east-1:573e1ed2-cc7e-4315-8714-199a170ea0d3',
-            Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": id_token}
-        });
-    let refresh = AWS.config.credentials.getPromise();
-    refresh.then(() => axios.get('', {params: {
-      Action: "getSigninToken",
-      Session: fixedEncodeURIComponent(`{"sessionId": "${AWS.config.credentials.accessKeyId}", "sessionKey": "${AWS.config.credentials.secretAccessKey}", "sessionToken": "${AWS.config.credentials.sessionToken}"}`)
-    }}).then(console.log))
-    refresh.catch(console.log);
+      AWS.config.region = 'us-east-1';
+      let id_token=hash().id_token;
+      AWS.config.credentials= new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-east-1:573e1ed2-cc7e-4315-8714-199a170ea0d3',
+        Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": id_token}});
+      let refresh = AWS.config.credentials.getPromise();
+      refresh.then(() => {
+        let lambda = AWS.Lambda();
+        lambda.invoke({
+          FunctionName: "cloud9-FederationBroker-getSigninUrl-JPLP6TPQKNJX",
+          Payload: {
+            sessionId: AWS.config.credentials.accessKeyId,
+            sessionKey: AWS.config.credentials.secretAccessKey,
+            sessionToken: AWS.config.credentials.sessionToken
+          }
+        }, (err, data) => {
+          window.response = {err: err, data: data}
+        })
+      })
     }
     catch (e) {
         console.log(e);
     }
-    
 }
