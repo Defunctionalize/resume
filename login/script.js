c15e0c2e735dcfe1eaffa1691a2ea586562cb584
Tyler Tolton <tjtolton@gmail.com>
2019-11-03 01:14:17 +0000

    login fragment vals

--- login/script.js
+++ login/script.js
@@ -8,13 +8,12 @@ function test() {
     try {
 	AWS.config.region = 'us-east-1';
 	let id_token=hash().id_token;
-	console.log('ID Token: ', id_token);
+	console.log('ID Token', id_token);
 	AWS.config.credentials= new AWS.CognitoIdentityCredentials({
             IdentityPoolId: 'us-east-1:573e1ed2-cc7e-4315-8714-199a170ea0d3',
             Logins:{"cognito-idp.us-east-1.amazonaws.com/us-east-1_iaCr8Wzlt": id_token}
         });
-    let refresh = AWS.config.credentials.getPromise();
-    refresh.then(() => console.log("creds? ---", AWS.console.credentials.accessKeyId));
+    AWS.config.credentials.get(() => console.log("creds? ---", AWS.console.credentials.accessKeyId));
     }
     catch (e) {
         console.log(e);
