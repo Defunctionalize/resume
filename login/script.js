c7567d101c954622a02ef42bdb96091b0c29d680
Tyler Tolton <tjtolton@gmail.com>
2019-11-03 02:29:55 +0000

    catch errors

--- login/script.js
+++ login/script.js
@@ -1,5 +1,12 @@
 var hash = () => Object.fromEntries(window.location.hash.substr(1).split("&").map(x => x.split("=")))
-
+var serialize = function(obj) {
+  var str = [];
+  for (var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
 function test() {
     console.log = (...x) => alert(x);
     console.error = console.log;
@@ -16,7 +23,10 @@ function test() {
     log_creds = () => {try {console.log(`{"access_key_id": "${AWS.config.credentials.accessKeyId}", "secret_access_key": "${AWS.config.credentials.secretAccessKey}", "session_token": "${AWS.config.credentials.sessionToken}"}`)} catch (e) {console.log(e)}}
     log_creds();
     let refresh = AWS.config.credentials.getPromise();
-    refresh.then(log_creds);
+    refresh.then(() => console.log(serialize({"sessionId": AWS.config.credentials.accessKeyId,
+                                              "sessionKey": AWS.config.credentials.secretAccessKey,
+                                              "sessionToken": AWS.config.credentials.sessionToken}))
+    })));
     refresh.catch(console.log);
     }
     catch (e) {
