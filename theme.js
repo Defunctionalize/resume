160e238cf07ba34a9c43b24833a6f12139ca4b72
Tyler Tolton <tjtolton@gmail.com>
2023-07-28 21:54:19 -0400

    resume update.  move ML to main, change build code, change theme

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 'use strict';
  
-var theme = require('jsonresume-theme-eloquent');
+var theme = require('jsonresume-theme-straightforward');
  
 var resume = require('./resume.json');
 process.stdout.write(theme.render(resume));
--- themeML.js
+++ themeML.js
@@ -1,6 +1,6 @@
 'use strict';
  
-var theme = require('jsonresume-theme-eloquent');
+var theme = require('npm install jsonresume-theme-straightforward');
  
 var resume = require('./resumeML.json');
 process.stdout.write(theme.render(resume));
