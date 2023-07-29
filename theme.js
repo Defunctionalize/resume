365306a3000b0f4e1717571180f8d2f989cf292b
Tyler Tolton <tjtolton@gmail.com>
2023-07-29 01:03:26 -0400

    restructure project, start changing resume content

--- theme.js
+++
@@ -1,6 +0,0 @@
-'use strict';
- 
-var theme = require('jsonresume-theme-straightforward');
- 
-var resume = require('./resume.json');
-process.stdout.write(theme.render(resume));
--- themeML.js
+++
@@ -1,6 +0,0 @@
-'use strict';
- 
-var theme = require('npm install jsonresume-theme-straightforward');
- 
-var resume = require('./resumeML.json');
-process.stdout.write(theme.render(resume));
